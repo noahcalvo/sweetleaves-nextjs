0f29358cfca40a5c0fad23125528c4ea387963ac
Noah Calvo <noah.calvo@gmail.com>
2026-03-28 16:10:48 -0500

    fix: use project color tokens and named height in SignInButton

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,19 @@ module.exports = {
     './lib/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-green':  '#0F2D25',
+        'dark-sage':   '#24433B',
+        'sage':        '#6D7E76',
+        'ivory':       '#E0D7B7',
+        'parchment':   '#F7F4EE',
+        'almost-black':'#211A1D',
+      },
+      height: {
+        'sign-in-modal': '600px',
+      },
+    },
   },
   plugins: [],
 };
