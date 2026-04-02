57d905dafa45f7d18777c95e1bce6f35093f5938
Noah Calvo <noah.calvo@gmail.com>
2026-04-01 20:27:37 -0500

    cleanup

--- app/components/home/DealsBanner.tsx
+++ app/components/home/DealsBanner.tsx
@@ -1,5 +1,5 @@
 export default function DealsBanner() {
   return (
-    <div className="bg-[#c4c4c4] rounded-[40px] h-[273px] md:h-[320px] w-full" />
+    <div className="bg-sage/30 rounded-[40px] h-[273px] md:h-[320px] w-full" />
   );
 }
