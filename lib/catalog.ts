5c22f7478ac79d6d9caf96212f35c12c46ced987
Noah Calvo <noah.calvo@gmail.com>
2026-04-05 21:15:40 -0500

    add CatalogEntry and AccordionItem types

---
+++ lib/catalog.ts
@@ -0,0 +1,16 @@
+export interface AccordionItem {
+  title: string;
+  content: string;
+}
+
+export interface CatalogEntry {
+  slug: string;
+  name: string;
+  heroImage: string;
+  headline: string;
+  accordionItems: AccordionItem[];
+  iframeUrl: string;
+  subheadline: string;
+  body: string;
+  metaDescription: string;
+}
